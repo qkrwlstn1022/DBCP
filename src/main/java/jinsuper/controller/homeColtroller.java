c24a66efbe20126946f7d46655a0ede386bf2aec
박진수 <qkrwlstn1022@naver.com>
2020-02-17 08:30:18 +0900

    컨트롤러 추가

---
+++ src/main/java/jinsuper/controller/homeColtroller.java
@@ -0,0 +1,13 @@
+package main.java.jinsuper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeColtroller {
+
+    @RequestMapping(value = "/")
+    public String showHome(){
+        return "home";
+    }
+}
