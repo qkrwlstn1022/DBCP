b48ae0b54cc690a1e7cc3aee1e4c0e29798e6edc
박진수 <qkrwlstn1022@naver.com>
2020-02-17 08:43:29 +0900

    기본 개발환경 구축

--- src/main/java/jinsuper/controller/homeColtroller.java
+++ src/main/java/jinsuper/controller/homeColtroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class homeColtroller {
 
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/home")
     public String showHome(){
         return "home";
     }
